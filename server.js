594989a7d3feac6a4466ad2893be153e49edfa8b
Heshan Padmasiri <hpheshan@gmail.com>
2019-04-20 19:37:00 +0530

    fixed typo in server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/AngularFirebaseCRUD'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AngularFirebaseCRUD/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
