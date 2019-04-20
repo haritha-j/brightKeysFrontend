2ed9b23d797925b095ad7bc1400d95302c8a2b31
haritha-j <harithajayasinghe@gmail.com>
2019-04-20 17:53:10 +0530

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC1ONYwXnlDRf8_yhbIO1RXyjYs2SKGceg",
+    authDomain: "brightkeys-254ba.firebaseapp.com",
+    databaseURL: "https://brightkeys-254ba.firebaseio.com",
+    projectId: "brightkeys-254ba",
+    storageBucket: "brightkeys-254ba.appspot.com",
+    messagingSenderId: "284731526876"
+  }
 };
