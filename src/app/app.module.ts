79ae17ef6891cfcfb2aced0e540292d63db286cf
Heshan Padmasiri <hpheshan@gmail.com>
2019-04-20 19:13:10 +0530

    heroku deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { OrderListComponent } from "./order-list/order-list.component";
-import { OrdersService } from "./shared/orders.service";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersService } from './shared/orders.service';
 
-//environment import
-import { environment } from "src/environments/environment";
+// environment import
+import { environment } from 'src/environments/environment';
 
-//angularfire imports
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
+// angularfire imports
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, OrderListComponent],
